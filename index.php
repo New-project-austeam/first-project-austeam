88a3e121dfb0a5106ddcb4b2a6faa69dfe08ef16
naoko318 <n318jp@gmail.com>
2021-02-21 15:21:05 +0900

    no message

--- index.php
+++ index.php
@@ -56,43 +56,50 @@ session_start();
     </section>
 
 
-    <section　class="instruction">
-      <div>
-        <div class="sec-title">
-          <h3>このサイトの使い方</h3>
-        </div>
+   <section class="main-contents">
+     <section class="instruction">
+       <div class="section-title">
+         <h3>このサイトの使い方</h3>
+       </div>
 
+       <div class="how-to">
+         <h4>Step 1</h4>
+         <p>
+           アカウント登録をして、マイページを作成！
+         </p>
+         <img src="./dist/images/computer_tablet_woman2.png" width="100px">
 
-        <div class="how-to">
-          <h4>Step 1</h4>
-          <img>
-          <p>マイアカウントを作る</p>
-        </div>
+       </div>
+
 
         <div class="how-to">
           <h4>Step 2</h4>
-          <img>
           <p>イベントに参加・作成</p>
+          <img src="./dist/images/calender_man.png" width="100px"　alt="イベントを開催、参加"　>
+
         </div>
 
         <div class="how-to">
           <h4>Step 3</h4>
-          <img>
           <p>準備　地方自治体のゴミ処分の仕方を確認。必要な持ち物を用意する。天候に合わせた服装</p>
+          <img src="./dist/images/kotowaza_kawaiiko_tabi_girl.png" width="100px"　alt="準備をしてイベントに参加">
+
         </div>
 
         <div class="how-to">
           <h4>Step 4</h4>
-          <img>
           <p>イベント後、あなたの経験を記録やシェア</p>
+          <img src="./dist/images/computer_hacker_white1_woman.png" width="100px" alt="イベントの経験をシェア">
+
         </div>
 
-      </div>
-    </section>
+    </section><!-- instruction -->
+
+
 
     <section class="new-event">
-      <div>
-      <h3 class="sec-title">新着のイベント</h3>
+      <div class="section-title">
+      <h3>新着のイベント</h3>
       </div>
 
       <div><!-- ここでイベントを反映させたい -->
@@ -104,7 +111,9 @@ session_start();
     </section>
 
     <section>
-      <h3 class="sec-title">ゴミマップに登録する</h3>
+    <div class="section-title">
+      <h3 >ゴミマップに登録する</h3>
+    </div>
       <button type="submit"><a href="#">Join Us</a></button>
       <div class="sns">
       <i class="fab fa-facebook"></i>
@@ -124,6 +133,8 @@ session_start();
         <li><a href="#">お問い合わせ</a></li>
       </uL>
       </div>
+
+      </section><!-- main-contents -->
 
 
 
