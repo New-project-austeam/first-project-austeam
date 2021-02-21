d45b6414a7d10cd00151f8ae3fd21d546d114a5b
naoko318 <n318jp@gmail.com>
2021-02-21 16:50:28 +0900

    footer以外のコンテンツ、更新 スマホバージョンのみ作成

--- index.php
+++ index.php
@@ -37,104 +37,130 @@ session_start();
       </ul>
     </div>
   </header>
+
   <main>
+
     <section class="top-visual">
 
       <div class="msg-1">
         <h2>ゴミ拾いで</h2>
-        <span  class="slide">
-        <h2>繋がる出会い</h2>
+        <span class="slide">
+          <h2>繋がる出会い</h2>
         </span>
       </div>
 
       <div class="msg-2">
         <h2>仲間と一緒に</h2>
-        <span  class="slide">
+        <span class="slide">
           <h2> 海を守りませんか？</h2>
-          </span>
+        </span>
       </div>
     </section>
 
 
-   <section class="main-contents">
-     <section class="instruction">
-       <div class="section-title">
-         <h3>このサイトの使い方</h3>
-       </div>
+    <section class="main-contents">
 
-       <div class="how-to">
-         <h4>Step 1</h4>
-         <p>
-           アカウント登録をして、マイページを作成！
-         </p>
-         <img src="./dist/images/computer_tablet_woman2.png" width="100px">
+        <span class="space">スペース</span>
 
-       </div>
+      <section class="instruction">
+
+        <div class="section-title">
+
+
+          <h3>HOW TO START</h3>
+          <p>このサイトの使い方</p>
+        </div>
+
+        <div class="how-to">
+          <h4 class="step-ribbon">Step 1</h4>
+          <div class="how-to-contents">
+            <p>
+              アカウント登録をして、マイページを作成！
+            </p>
+            <img src="./dist/images/computer_tablet_woman2.png" width="100px">
+          </div>
+
+        </div>
 
 
         <div class="how-to">
-          <h4>Step 2</h4>
-          <p>イベントに参加・作成</p>
-          <img src="./dist/images/calender_man.png" width="100px"　alt="イベントを開催、参加"　>
+          <h4 class="step-ribbon">Step 2</h4>
+          <div class="how-to-contents">
+            <p>イベントに参加・作成</p>
+            <img src="./dist/images/calender_man.png" width="100px" 　alt="イベントを開催、参加" 　>
+
+          </div>
 
         </div>
 
         <div class="how-to">
-          <h4>Step 3</h4>
-          <p>準備　地方自治体のゴミ処分の仕方を確認。必要な持ち物を用意する。天候に合わせた服装</p>
-          <img src="./dist/images/kotowaza_kawaiiko_tabi_girl.png" width="100px"　alt="準備をしてイベントに参加">
+          <h4 class="step-ribbon">Step 3</h4>
+          <div class="how-to-contents">
+            <p>準備　地方自治体のゴミ処分の仕方を確認。必要な持ち物を用意する。天候に合わせた服装</p>
+            <img src="./dist/images/kotowaza_kawaiiko_tabi_girl.png" width="100px" 　alt="準備をしてイベントに参加">
+          </div>
 
         </div>
 
         <div class="how-to">
-          <h4>Step 4</h4>
-          <p>イベント後、あなたの経験を記録やシェア</p>
-          <img src="./dist/images/computer_hacker_white1_woman.png" width="100px" alt="イベントの経験をシェア">
+          <h4 class="step-ribbon">Step 4</h4>
+          <div class="how-to-contents">
+            <p>イベント後、あなたの経験を記録やシェア</p>
+            <img src="./dist/images/computer_hacker_white1_woman.png" width="100px" alt="イベントの経験をシェア">
+          </div>
 
         </div>
 
-    </section><!-- instruction -->
+      </section><!-- instruction -->
+      <span class="space">スペース</span>
 
 
 
-    <section class="new-event">
-      <div class="section-title">
-      <h3>新着のイベント</h3>
-      </div>
+      <section class="new-event">
+        <div class="section-title">
+          <h3>NEW EVENT</h3>
+          <p>新着のイベント</p>
+        </div>
 
-      <div><!-- ここでイベントを反映させたい -->
-        <img>
-        <img>
-        <img>
-        <img>
-      </div>
-    </section>
+        <div>
+          <!-- ここでイベントを反映させたい -->
+          <img>
+          <img>
+          <img>
+          <img>
+        </div>
+      </section>
+      <span class="space">スペース</span>
 
-    <section>
-    <div class="section-title">
-      <h3 >ゴミマップに登録する</h3>
-    </div>
-      <button type="submit"><a href="#">Join Us</a></button>
-      <div class="sns">
-      <i class="fab fa-facebook"></i>
-      <i class="fab fa-twitter"></i>
-      <i class="fab fa-instagram-square"></i>
+      <section class="registration">
+        <div class="section-title">
+          <h3>JOIN US</h3>
+          <p>ゴミマップに登録する</p>
+        </div>
 
-      </div>
-    </section>
+        <div class="join-button">
+          <button type="submit"><a href="#">Join Us</a></button>
+        </div>
+        <div class="sns">
+          <i class="fab fa-facebook"></i>
+          <i class="fab fa-twitter"></i>
+          <i class="fab fa-instagram-square"></i>
 
-    <footer>
-      <div>
-      <h5 class="logo"><a href="index.php">ゴミ<i class="fas fa-trash-alt"></i>マップ</a></h5>
+        </div>
+      </section>
 
-      <uL>
-        <li><a href="#">サイトポリシー</a></li>
-        <li><a href="#">サイト設立の経緯</a></li>
-        <li><a href="#">お問い合わせ</a></li>
-      </uL>
-      </div>
+      <footer>
+        <div>
+          <h5 class="logo"><a href="index.php">ゴミ<i class="fas fa-trash-alt"></i>マップ</a></h5>
 
-      </section><!-- main-contents -->
+          <uL>
+            <li><a href="#">サイトポリシー</a></li>
+            <li><a href="#">サイト設立の経緯</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+          </uL>
+        </div>
+
+    </section><!-- main-contents -->
 
 
 
