2de742b6d32bf663a65f106d7ce171b2f14604fc
naoko318 <n318jp@gmail.com>
2021-03-14 15:02:52 +0900

    timeline.php・scssの修正

--- App/Views/Home/include/timeline.php
+++ App/Views/Home/include/timeline.php
@@ -1,71 +1,168 @@
-<main class="timeline-wrapper">
+<main class="bg-img">
   <span class="space">スペース</span>
 
-  <div class="timeline-contents">
+  <div class="timeline-wrapper">
 
-    <aside class="recommendation">
-      <h3>ゴミマップのおすすめ</h3>
-    </aside>
 
-    <section class="timeline">
+    <aside class="search timeline-wrap">
+      <h2 class="aside-title"><i class="fas fa-trash-alt"></i> Search</h2>
+      <form>
 
-      <form class="search">
-        <dl>
-          <div>
-            <dt>イベント検索</dt>
-            <dd><input type="search"></dd>
-            <input type="button" value="検索">
-          </div>
+        <dl class="search-list">
+          <dd><input type="search" placeholder="イベント検索">
+            <input type="submit" value="検索">
+          </dd>
 
-          <div>
-            <dt>日付検索</dt>
-            <dd><input type="date"></dd>
-            <input type="button" value="検索">
-          </div>
+          <dd><input type="date">
+            <input type="submit" value="検索">
+          </dd>
         </dl>
       </form>
+      <div class="recommendation">
+        <h3 class="aside-title"><i class="fas fa-trash-alt"></i> ゴミマップのおすすめ</h3>
+      </div>
 
-      <div class="posted-event">
-        <diV class="post-event-top">
-          <div class="icon-border">
-            <img src="<?php echo URLROOT; ?>/dist/images/woman-332278_1920.jpg" width="100px"><!-- アカウントのアイコン　丸 -->
+      <div class="source-quote"><a href='https://www.freepik.com/vectors/background'>Background vector created by freepik - www.freepik.com</a></div>
+    </aside><!-- recommendation -->
+
+
+
+
+
+    <section class="timeline timeline-wrap">
+
+
+      <section class="wanted">
+        <i class="fas fa-trash-alt"></i>
+        　　　　　<form class="filter">
+
+          <select>
+          <option></option>
+            <option selected>近日中のイベント</option>
+            <option>新着の投稿</option>
+            <option>イベント報告</option>
+          </select>
+        </form>
+
+
+
+        <div class="posted">
+
+          <div class="posted-event">     <!-- flex -->
+
+            <div class="icon">
+              <div class="icon-border">
+                <img src="<?php echo URLROOT; ?>/dist/images/woman-332278_1920.jpg">
+              </div>
+              <p class="nickname"><a href="#">name</a></p>
+            </div>
+
+
+            <div class="poseted-event-contents">
+              <div class="posted-event-title">     <!-- flex -->
+              <div class="event-title">
+
+                <h4><a>急募!!海岸でのごみ拾い員募集</a></h4>
+                </div>
+                <div class="favorite-icon">
+                   <h3><a>☆</a></h3><!-- 仮 -->
+                </div>
+              </div><!-- イベントタイトル -->
+
+
+              <ul class="event-info-list">     <!-- flex -->
+                <li><i class="fas fa-trash-alt"></i> 開催日:</li>
+                <li><i class="fas fa-trash-alt"></i> 場所:</li>
+                <li><i class="fas fa-trash-alt"></i> 環境テーマ:</li>
+              </ul>
+
+
+
+            </div><!-- posted-event-contents -->
+
+
+
           </div>
-          <div class="event-title">
-            <h4>海岸のごみ拾い募集</h4><!-- イベントタイトル -->
-            <ul>
-              <li>日時：2020/9/30</li>
-              <li>環境テーマ：<a href="#">ごみ拾い</a></li>
-            </ul>
-          </div><!-- event-title -->
-        </diV><!-- post-event-top -->
+          <!--class="posted-event"  -->
 
-        <div class="summary">
-          <div class="event-summary">
-            <p>Viewport Height（vh）：viewportの高さに基づく。1vhはviewportの高さの1%に相当する
-              Viewport Width（vw）：viewportの幅に基づく。1vwはviewportの幅の1%に相当する...続きは
+          <div  class="event-detail">
+              <span style="font-weight: bold;">詳細:</span>
+              <p> 突如、大量に漂流ゴミが海岸に打ち上げられてるため清掃必須..突如、大量に漂流ゴミが海岸に打ち上げられてるため清掃必須..</p>
+              </div>
 
-
-
-            </p>
-          </div>
-          <div>
-            <img><!-- イメージ画像、任意 -->
+              <div  class="event-detail">
+              <span style="font-weight: bold;">参加予定:</span>
+              <p>3人</p>
+              </div>
+          <div class="detail">
+          <button class="detail-btn"><a>詳細</a></button>
           </div>
 
-          <div>
-            <p>いいね！</p>
-            <p>気になる！</p>
-            <p>３人が参加予定</p>
+
+
+
+
+        </div><!-- posted-->
+      </section><!-- wanted -->
+
+
+      <section class="record-contents">
+      <section class="record">
+
+      <div class="posted">
+          <div class="posted-event">     <!-- flex -->
+            <div class="icon">
+            <div class="icon-border">
+                <img src="<?php echo URLROOT; ?>/dist/images/woman-332278_1920.jpg">
+              </div>
+              <p class="nickname"><a href="#">name</a></p>
+            </div>
+
+
+            <div class="poseted-event-contents">
+              <div class="posted-event-title">     <!-- flex -->
+              <div class="event-title">
+              <h4><a href="#">"清掃してきました"</a></h4>
+                </div>
+                <div class="favorite-icon">
+                   <h3><a>☆</a></h3><!-- 仮 -->
+                </div>
+              </div><!-- イベントタイトル -->
+              <p class="record-summary">台風１０号により被害が大きかった地域での清掃活動を行ってきました！台風１０号により被害が大きかった地域での清掃活動を行ってきました！</p>
+            </div><!-- posted-event-contents -->
+
+
+          </div>
+          <!--class="posted-event"  -->
+
+          <div class="detail">
+          <button class="detail-btn"><a>詳細</a></button>
           </div>
 
-          <div>
-            <textarea placeholder="コメント入力"></textarea>
-          </div>
-        </div><!-- event-summary -->
-      </div><!-- summary -->
 
-      <div><a href='https://www.freepik.com/photos/background'>Background photo created by rawpixel.com - www.freepik.com</a></div>
-    </section> <!-- timeline -->
+
+
+
+        </div><!-- posted-->
+      </section> <!-- record -->
+
+      </section>
+
+
+
+
+
+
+
+    </section><!-- timeline -->
+
+
+
+  </div><!-- timeline-wrapper -->
+
+
+
+
 
 
 
@@ -73,38 +170,36 @@
   </div><!-- timeline-contents -->
 
   <div class="wrap myevents">
-   <?php
+    <?php
     flash('register_new_event');
     ?>
-   <h2>近日中のイベント</h2>
+    <h2>近日中のイベント</h2>
 
-   <?php //print_r($data['myposts']);
+    <?php //print_r($data['myposts']);
     ?>
 
-   <div class="myevents_box">
+    <div class="myevents_box">
 
-     <?php
+      <?php
       $myposts = $data['allPosts'];
       foreach ($myposts as $post) {
 
       ?>
-     <a href="#" class="myevent_list">
+        <a href="#" class="myevent_list">
 
-       <p>
-         <span class="title">イベントタイトル</span><?php echo $post->event_title ?><span
-           class="title mg-l">開催日</span><?php echo $post->event_date; ?><span
-           class="title mg-l">イベント作成日</span><?php echo $post->created_at; ?>
-       </p>
-       <p><span class="title">詳細</span><?php echo $post->event_details; ?></p>
+          <p>
+            <span class="title">イベントタイトル</span><?php echo $post->event_title ?><span class="title mg-l">開催日</span><?php echo $post->event_date; ?><span class="title mg-l">イベント作成日</span><?php echo $post->created_at; ?>
+          </p>
+          <p><span class="title">詳細</span><?php echo $post->event_details; ?></p>
 
-     </a>
+        </a>
 
-     <?php
+      <?php
       }
       ?>
 
-   </div>
- </div>
+    </div>
+  </div>
 
 
 
